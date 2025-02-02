a093b343ba04d9b83d87c87de0dce93b81e975d0
silver0108 <kimsoeun0108@gmail.com>
2025-02-03 01:01:30 +0900

    파일 분리 및 리팩토링

---
+++ data/info.js
@@ -0,0 +1,20 @@
+export const messages = {
+  major:
+    "제 주전공은 '정보보안암호수학과'이고, 복수전공은 '소프트웨어전공' 입니다. 🎓",
+  mbti: `<div><span>MBTI는 'ISFJ' 입니다.</span> <img src="./assets/icons/mbti.png" alt="mbti"/></div>`,
+  exp: "지금까지 제일 좋았던 경험은.. 혼자서 간 몽골여행 ! 밤하늘의 별이 장난아니에요..🥹🌌",
+  hobby: `요즘 취미는 다꾸(다이어리 꾸미기)👑이고, 관심사는 요리👩🏻‍🍳 입니다 ~ <br/><br/>집에서 밥 해먹으려고 노력하다 보니 유튜브에 레시피 영상들이 많이 나오는 요즘이에요 ㅎ`,
+  plan: "제발 올해 안에 취업하기 ! ! ! <br/> 벌써 1년 째 취준 중이네여...😭",
+  contact: `<div><img src="./assets/icons/github.png" alt="github"/> <a href="https://github.com/silver0108" target="_blank">https://github.com/silver0108</a></div><br/>
+      <div><img src="./assets/icons/tistory.svg" alt="tistory"/> <a href="https://nueos.tistory.com" target="_blank">https://nueos.tistory.com</a></div><br/>
+      <div><img src="./assets/icons/instagram.png" alt="instagram"/> <a href="https://www.instagram.com/_nue_os" target="_blank">https://www.instagram.com/_nue_os</a></div>`,
+};
+
+export const images = {
+  major: "",
+  mbti: "",
+  exp: `<img src="./assets/images/mongolia1.jpeg" alt="몽골1"/> <img src="./assets/images/mongolia2.jpeg" alt="몽골2"/>`,
+  hobby: "",
+  plan: "",
+  contact: "",
+};
--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-const messages = {
-  major:
-    "제 주전공은 '정보보안암호수학과'이고, 복수전공은 '소프트웨어전공' 입니다. 🎓",
-  mbti: `<div><span>MBTI는 'ISFJ' 입니다.</span> <img src="./mbti.png" alt="mbti"/></div>`,
-  exp: "지금까지 제일 좋았던 경험은.. 혼자서 간 몽골여행 ! 밤하늘의 별이 장난아니에요..🥹🌌",
-  hobby: `요즘 취미는 다꾸(다이어리 꾸미기)👑이고, 관심사는 요리👩🏻‍🍳 입니다 ~ <br/><br/>집에서 밥 해먹으려고 노력하다 보니 유튜브에 레시피 영상들이 많이 나오는 요즘이에요 ㅎ`,
-  plan: "제발 올해 안에 취업하기 ! ! ! <br/> 벌써 1년 째 취준 중이네여...😭",
-  contact: `<div><img src="./github.png" alt="github"/> <a href="https://github.com/silver0108" target="_blank">https://github.com/silver0108</a></div><br/>
-    <div><img src="./tistory.svg" alt="tistory"/> <a href="https://nueos.tistory.com" target="_blank">https://nueos.tistory.com</a></div>`,
-};
-
-const images = {
-  major: "",
-  mbti: "",
-  exp: `<img src="./mongolia1.jpeg" alt="몽골1"/> <img src="./mongolia2.jpeg" alt="몽골2"/>`,
-  hobby: "",
-  plan: "",
-  contact: "",
-};
+import { messages, images } from "./data/info.js";
 
 const appWrapper = document.querySelector(".app-wrapper");
 const appElements = document.querySelectorAll(".app");
@@ -49,3 +31,5 @@ function refreshPage() {
   imageBox.style.display = "none";
   imageBox.classList.remove("visible");
 }
+
+window.refreshPage = refreshPage; // script.js가 type="module"로 불러와져 있기 때문에 window 객체에 등록하여 HTML에서 호출되도록 전역 함수 등록 처리
