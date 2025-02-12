e53e38039d4f28c44a1714db546f0835dde50263
silver0108 <kimsoeun0108@gmail.com>
2025-02-12 09:13:18 +0900

    사진 및 메시지 수정

--- data/info.js
+++ data/info.js
@@ -1,10 +1,9 @@
 export const messages = {
-  major:
-    "제 주전공은 '정보보안암호수학과'이고, 복수전공은 '소프트웨어전공' 입니다. 🎓",
+  major: "🎓 주전공: '정보보안암호수학과'<br/>👩🏻‍💻 복수전공: '소프트웨어전공'",
   mbti: `<div><span>MBTI는 'ISFJ' 입니다.</span> <img src="./assets/icons/mbti.png" alt="mbti"/></div>`,
-  exp: "지금까지 제일 좋았던 경험은.. 혼자서 간 몽골여행 ! 밤하늘의 별이 장난아니에요..🥹🌌",
-  hobby: `요즘 취미는 다꾸(다이어리 꾸미기)👑이고, 관심사는 요리👩🏻‍🍳 입니다 ~ <br/><br/>집에서 밥 해먹으려고 노력하다 보니 유튜브에 레시피 영상들이 많이 나오는 요즘이에요 ㅎ`,
-  plan: "유레카 무사히 수료해서 제발 올해 안에 취업하기 ! ! ! <br/> 벌써 1년 째 취준 중이기에 😭 마지막 기회라고 생각하면서 열심히 할 예정입니다!",
+  exp: "지금까지 제일 좋았던 경험은.. 혼자서 간 몽골여행 ! 밤하늘의 별이 장난 아니에요..🥹🌌",
+  hobby: `요즘 취미는 다꾸(다이어리 꾸미기)👑이고, <br/>관심사는 요리👩🏻‍🍳 입니다 ~ <br/><br/> 🎮 게임도 좋아하고 ott나 유튜브 📺 보는 것도 좋아합니다 :)`,
+  plan: "유레카 무사히 수료해서 제발 올해 안에 취업하기 ! ! ! <br/> 취준이 길어지고 있어 😭 마지막 기회라고 생각하면서 열심히 할 예정입니다!",
   contact: `<div><img src="./assets/icons/github.png" alt="github"/> <a href="https://github.com/silver0108" target="_blank">https://github.com/silver0108</a></div><br/>
       <div><img src="./assets/icons/tistory.svg" alt="tistory"/> <a href="https://nueos.tistory.com" target="_blank">https://nueos.tistory.com</a></div><br/>
       <div><img src="./assets/icons/instagram.png" alt="instagram"/> <a href="https://www.instagram.com/_nue_os" target="_blank">https://www.instagram.com/_nue_os</a></div>`,
